febce2174268787d1d470c2ecbaccf6ccfe49460
Antonio Quaresma <tozequa@gmail.com>
2023-12-29 10:15:50 +0000

    Setting basic header and footer components

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer>
+        <h2>Footer</h2>
+    </footer>
+</template>
+  
+<script>
+export default {
+    
+    // Composition API logic goes here
+};
+</script>
+  
+<style scoped>
+/* CSS for your header */
+</style>
+  
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <h2>Header</h2>
+    </header>
+</template>
+  
+<script>
+export default {
+    
+    // Composition API logic goes here
+};
+</script>
+  
+<style scoped>
+/* CSS for your header */
+</style>
+  
