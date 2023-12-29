293230a630662f84decf8b8b8773d2ec7c0968d9
Antonio Quaresma <tozequa@gmail.com>
2023-12-29 11:09:57 +0000

    building and styling the footer component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
-<the-header></the-header>
-<the-footer></the-footer>
+  <the-header></the-header>
+  <!-- pages content here -->
+  <the-footer></the-footer>
 </template>
+
 
 <script>
 import TheHeader from './components/TheHeader.vue';
@@ -20,89 +22,89 @@ export default {
 *,
 *::before,
 *::after {
-    box-sizing: border-box;
+  box-sizing: border-box;
 }
 
 /* Custom properties: */
 
 :root {
-    /* - Font family */
-    --ff-primary: 'Source Sans Pro', sans-serif;
-    --ff-secondary: 'Source Code Pro', monospace;
+  /* - Font family */
+  --ff-primary: 'Source Sans Pro', sans-serif;
+  --ff-secondary: 'Source Code Pro', monospace;
 
-    /* - Font weight */
-    --fw-reg: 300;
-    --fw-bold: 900;
+  /* - Font weight */
+  --fw-reg: 300;
+  --fw-bold: 900;
 
-    /* - Font sizes */
-    --fs-h1: 3rem;
-    --fs-h2: 2.25rem;
-    --fs-h3: 1.25rem;
-    --fs-body: 1rem;
+  /* - Font sizes */
+  --fs-h1: 3rem;
+  --fs-h2: 2.25rem;
+  --fs-h3: 1.25rem;
+  --fs-body: 1rem;
 
-    /* - Colors */
-    --clr-light: #fff;
-    --clr-dark: #303030;
-    --clr-accent: #16e0bd ; 
+  /* - Colors */
+  --clr-light: #fff;
+  --clr-dark: #303030;
+  --clr-accent: #16e0bd;
 
-    /* box shadow */
-    --bs: 0.25em 0.25em 0.75em rgba(0,0,0,.25),
-          0.125em 0.125em 0.25em rgba(0,0,0,.15);
+  /* box shadow */
+  --bs: 0.25em 0.25em 0.75em rgba(0, 0, 0, .25),
+    0.125em 0.125em 0.25em rgba(0, 0, 0, .15);
 
 }
 
 @media (min-width: 800px) {
-    :root {
-        /* - Font sizes */
-        --fs-h1: 4.5rem;
-        --fs-h2: 3.75rem;
-        --fs-h3: 1.5rem;
-        --fs-body: 1.125rem;
-    }
+  :root {
+    /* - Font sizes */
+    --fs-h1: 4.5rem;
+    --fs-h2: 3.75rem;
+    --fs-h3: 1.5rem;
+    --fs-body: 1.125rem;
+  }
 }
 
 
 /* General Styles */
 
 html {
-    scroll-behavior: smooth;
+  scroll-behavior: smooth;
 }
 
-body{
-    background: var(--clr-light);
-    color: var(--clr-dark);
-    margin: 0;
-    font-family: var(--ff-primary);
-    font-size: var(--fs-body);
-    line-height: 1.6;
+body {
+  background: var(--clr-light);
+  color: var(--clr-dark);
+  margin: 0;
+  font-family: var(--ff-primary);
+  font-size: var(--fs-body);
+  line-height: 1.6;
 }
 
 section {
-    padding: 5em 2em;
+  padding: 5em 2em;
 }
 
-img{
-    display: block;
-    max-width: 100%;
+img {
+  display: block;
+  max-width: 100%;
 }
 
-strong{
-    font-weight: var(--fw-bold) ;
+strong {
+  font-weight: var(--fw-bold);
 }
 
-a{
-    color: inherit;
-    text-decoration: none;
-    font-weight: var(--fw-bold);
+a {
+  color: inherit;
+  text-decoration: none;
+  font-weight: var(--fw-bold);
 }
 
-a:hover{
-    color: var(--clr-accent);
+a:hover {
+  color: var(--clr-accent);
 }
 
 :focus {
-    outline: 3px solid var(--clr-accent);
-    outline-offset: 3px;
+  outline: 3px solid var(--clr-accent);
+  outline-offset: 3px;
 }
 
 /* Typography */
@@ -110,12 +112,19 @@ a:hover{
 h1,
 h2,
 h3 {
-    line-height: 1;
-    margin: 0;
+  line-height: 1;
+  margin: 0;
 }
 
-h1 { font-size: var(--fs-h1) }
-h2 { font-size: var(--fs-h2) }
-h3 { font-size: var(--fs-h3) }
+h1 {
+  font-size: var(--fs-h1)
+}
 
+h2 {
+  font-size: var(--fs-h2)
+}
+
+h3 {
+  font-size: var(--fs-h3)
+}
 </style>
--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,17 +1,80 @@
 <template>
-    <footer>
-        <h2>Footer</h2>
+    <footer class="footer">
+        <a href="#top" class="go-top__footer"><i class="fa-regular fa-circle-up"></i></a>
+        <a href="mailto:tozequa@gmail.com" class="footer__link">Email Me</a>
+        <ul class="social-list">
+
+            <li class="social-list__item">
+                <a class="social-list__link" href="https://github.com/AntQua" target="_blank">
+                    <i class="fab fa-github"></i>
+                </a>
+            </li>
+
+            <li class="social-list__item">
+                <a class="social-list__link" href="https://www.linkedin.com/in/antonio-quaresma/" target="_blank">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+            </li>
+        </ul>
     </footer>
 </template>
   
-<script>
+<!-- <script>
 export default {
-    
+
     // Composition API logic goes here
 };
-</script>
+</script> -->
   
 <style scoped>
-/* CSS for your header */
+.footer {
+    background: #111;
+    color: var(--clr-accent);
+    text-align: center;
+    padding: 2.5em 0;
+    font-size: var(--fs-h3);
+    position: relative;
+} 
+
+.go-top__footer {
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 10px 20px;
+}
+
+.footer a {
+    color: inherit;
+    text-decoration: none;
+}
+
+.footer__link {
+    font-weight: var(--fw-bold);
+}
+
+.footer__link:hover,
+.social-list__link:hover {
+    opacity: .7;
+}
+
+.footer__link:hover {
+    text-decoration: underline;
+}
+
+.social-list {
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    margin: 2em 0 0;
+    padding: 0;
+}
+
+.social-list__item {
+    margin: 0 0.5em;
+}
+
+.social-list__link {
+    padding: 0.5em;
+}
 </style>
   
