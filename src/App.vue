b1fbc442ecfc5408b7bf43ce7efca3edb2071e00
Antonio Quaresma <tozequa@gmail.com>
2023-12-29 10:48:37 +0000

    building the header with logo and nav menu with links

--- src/App.vue
+++ src/App.vue
@@ -17,5 +17,105 @@ export default {
 </script>
 
 <style>
-/* Global styles */
+*,
+*::before,
+*::after {
+    box-sizing: border-box;
+}
+
+/* Custom properties: */
+
+:root {
+    /* - Font family */
+    --ff-primary: 'Source Sans Pro', sans-serif;
+    --ff-secondary: 'Source Code Pro', monospace;
+
+    /* - Font weight */
+    --fw-reg: 300;
+    --fw-bold: 900;
+
+    /* - Font sizes */
+    --fs-h1: 3rem;
+    --fs-h2: 2.25rem;
+    --fs-h3: 1.25rem;
+    --fs-body: 1rem;
+
+    /* - Colors */
+    --clr-light: #fff;
+    --clr-dark: #303030;
+    --clr-accent: #16e0bd ; 
+
+    /* box shadow */
+    --bs: 0.25em 0.25em 0.75em rgba(0,0,0,.25),
+          0.125em 0.125em 0.25em rgba(0,0,0,.15);
+
+}
+
+@media (min-width: 800px) {
+    :root {
+        /* - Font sizes */
+        --fs-h1: 4.5rem;
+        --fs-h2: 3.75rem;
+        --fs-h3: 1.5rem;
+        --fs-body: 1.125rem;
+    }
+}
+
+
+/* General Styles */
+
+html {
+    scroll-behavior: smooth;
+}
+
+body{
+    background: var(--clr-light);
+    color: var(--clr-dark);
+    margin: 0;
+    font-family: var(--ff-primary);
+    font-size: var(--fs-body);
+    line-height: 1.6;
+}
+
+section {
+    padding: 5em 2em;
+}
+
+img{
+    display: block;
+    max-width: 100%;
+}
+
+strong{
+    font-weight: var(--fw-bold) ;
+}
+
+a{
+    color: inherit;
+    text-decoration: none;
+    font-weight: var(--fw-bold);
+}
+
+a:hover{
+    color: var(--clr-accent);
+}
+
+:focus {
+    outline: 3px solid var(--clr-accent);
+    outline-offset: 3px;
+}
+
+/* Typography */
+
+h1,
+h2,
+h3 {
+    line-height: 1;
+    margin: 0;
+}
+
+h1 { font-size: var(--fs-h1) }
+h2 { font-size: var(--fs-h2) }
+h3 { font-size: var(--fs-h3) }
+
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,17 +1,162 @@
 <template>
-    <header>
-        <h2>Header</h2>
+    <header :class="{ 'nav-open': isNavOpen }">
+        <div class="logo">
+            <img src="../assets/logo2.png" alt="Logo">
+        </div>
+        <button class="nav-toggle" aria-label="toggle navigation" @click="toggleNav">
+            <span class="hamburger"></span>
+        </button>
+        <nav class="nav">
+            <ul class="nav__list">
+                <li class="nav__item" v-for="link in navLinks" :key="link.name">
+                    <a href="#home" class="nav__link" @click="closeNav">{{ link.name }}</a>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
-  
+
 <script>
+import { ref } from 'vue';
+
 export default {
-    
-    // Composition API logic goes here
+    name: 'TheHeader',
+    setup() {
+        const isNavOpen = ref(false);
+
+        function toggleNav() {
+            isNavOpen.value = !isNavOpen.value;
+        }
+
+        function closeNav() {
+            isNavOpen.value = false;
+        }
+
+        const navLinks = [
+            { name: 'Home', href: '#home' },
+            { name: 'What I Do', href: '#skills' },
+            { name: 'About me', href: '#about' },
+            { name: 'My Projects', href: '#projects' }
+        ];
+
+        return { isNavOpen, toggleNav, closeNav, navLinks };
+    }
 };
 </script>
-  
+
 <style scoped>
-/* CSS for your header */
-</style>
+header {
+    display: flex;
+    justify-content: space-between;
+    padding: 1em;
+}
+
+.logo {
+    max-width: 120px;
+}
+
+.nav {
+    position: fixed;
+    background: var(--clr-dark);
+    color: var(--clr-light);
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 100;
+    opacity: 95%;
+
+    transform: translateX(100%);
+    transition: transform 250ms cubic-bezier(.5, 0, .5, 1);
+    ;
+}
+
+.nav__list {
+    list-style: none;
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    margin: 0;
+    padding: 0;
+}
+
+
+.nav__link {
+    color: inherit;
+    font-weight: var(--fw-bold);
+    font-size: var(--fs-h2);
+    text-decoration: none;
+}
+
+.nav__link:hover {
+    color: var(--clr-accent);
+}
+
+.nav-toggle {
+    padding: .5em;
+    background: transparent;
+    border: 0;
+    cursor: pointer;
+    position: absolute;
+    right: 1em;
+    top: 1em;
+    z-index: 1000;
+}
+
+.nav-open .nav {
+    transform: translateX(0);
+}
+
+.nav-open .nav-toggle {
+    position: fixed;
+}
+
+.nav-open .hamburger {
+    transform: rotate(.625turn);
+    background: var(--clr-accent);
+}
+
+.nav-open .hamburger::before {
+    transform: rotate(90deg) translateX(-6px);
+    background: var(--clr-accent);
+}
+
+.nav-open .hamburger::after {
+    opacity: 0;
+    background: var(--clr-accent);
+}
+
+.hamburger {
+    display: block;
+    position: relative;
+}
+
+.hamburger,
+.hamburger::before,
+.hamburger::after {
+    background: var(--clr-dark);
+    width: 2em;
+    height: 3px;
+    border-radius: 1em;
+    transition: transform 250ms ease-in-out;
+}
+
+
+.hamburger::before,
+.hamburger::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    right: 0;
+}
+
+.hamburger::before {
+    top: 6px;
+}
+
+.hamburger::after {
+    bottom: 6px;
+}</style>
   
